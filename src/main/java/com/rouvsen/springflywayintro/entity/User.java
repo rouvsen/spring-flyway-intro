e9c64bc3eb3c1a3ee69b8f5e6154b40ae6388aec
rovshan <rouvsen@gmail.com>
2023-05-11 00:50:16 +0400

    User Entity added

---
+++ src/main/java/com/rouvsen/springflywayintro/entity/User.java
@@ -0,0 +1,23 @@
+package com.rouvsen.springflywayintro.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    private Long id;
+    private String username;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+}
