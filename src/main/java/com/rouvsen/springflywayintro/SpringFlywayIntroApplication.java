8d4ac3667fdfa64015ea3218eac33fccd2e8dd5d
rovshan <rouvsen@gmail.com>
2023-05-11 00:49:44 +0400

    initial commit

---
+++ src/main/java/com/rouvsen/springflywayintro/SpringFlywayIntroApplication.java
@@ -0,0 +1,13 @@
+package com.rouvsen.springflywayintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFlywayIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFlywayIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rouvsen/springflywayintro/SpringFlywayIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rouvsen.springflywayintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlywayIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
