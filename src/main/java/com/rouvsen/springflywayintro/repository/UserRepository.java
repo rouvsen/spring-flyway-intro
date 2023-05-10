e2cac24d9ac7d8583c09b955136ba83da0aa8fcf
rovshan <rouvsen@gmail.com>
2023-05-11 00:50:31 +0400

    User Repository added

---
+++ src/main/java/com/rouvsen/springflywayintro/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.rouvsen.springflywayintro.repository;
+
+import com.rouvsen.springflywayintro.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository
+        extends JpaRepository<User, Long> {
+}
