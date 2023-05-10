19e9bf393af2c8c65dddd80e8154444d8ff5890e
rovshan <rouvsen@gmail.com>
2023-05-11 00:50:23 +0400

    User Controller added

---
+++ src/main/java/com/rouvsen/springflywayintro/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.rouvsen.springflywayintro.controller;
+
+import com.rouvsen.springflywayintro.entity.User;
+import com.rouvsen.springflywayintro.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+}
