c6fcc983d077606d2a0ab99335b0f29f1e666b7f
rovshan <rouvsen@gmail.com>
2023-05-11 18:05:52 +0400

    Product Api/Repository

---
+++ src/main/java/com/rouvsen/springflywayintro/apiversioning/api/ProductApi.java
@@ -0,0 +1,78 @@
+package com.rouvsen.springflywayintro.apiversioning.api;
+
+import com.rouvsen.springflywayintro.apiversioning.dto.ProductV1;
+import com.rouvsen.springflywayintro.apiversioning.dto.ProductV2;
+import com.rouvsen.springflywayintro.apiversioning.entity.Product;
+import com.rouvsen.springflywayintro.apiversioning.repo.ProductRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value = "/api")
+public class ProductApi {
+
+    private final ProductRepository productRepository;
+
+    public ProductApi(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    //URI Versioning
+
+    @GetMapping(value = "/v1/product")
+    public ResponseEntity<List<ProductV1>> pathVersioningProductV1() {
+        List<Product> all = productRepository.findAll();
+        List<ProductV1> allV1 = new ArrayList<>();
+        for (Product product : all) {
+            allV1.add(new ProductV1(
+                    product.getId(),
+                    product.getName())
+            );
+        }
+        return ResponseEntity.of(Optional.of(allV1));
+    }
+    @GetMapping(value = "/v2/product")
+    public ResponseEntity<List<ProductV2>> pathVersioningProductV2() {
+        List<Product> all = productRepository.findAll();
+        List<ProductV2> allV2 = new ArrayList<>();
+        for (Product product : all) {
+            allV2.add(new ProductV2(
+                    product.getId(),
+                    product.getName(),
+                    product.getPrice())
+            );
+        }
+        return ResponseEntity.of(Optional.of(allV2));
+    }
+
+    //Path Versioning
+    //http://localhost:8080/api/param/product?apiVersion=1
+    @GetMapping(value = "/param/product", params = "apiVersion=1")
+    public ResponseEntity<ProductV1> paramVersioningProductV1() {
+        return ResponseEntity.of(Optional.of(new ProductV1("HP Laptop")));
+    }
+    //http://localhost:8080/api/param/product?apiVersion=2
+    @GetMapping(value = "/param/product", params = "apiVersion=2")
+    public ResponseEntity<ProductV2> paramVersioningProductV2() {
+        return ResponseEntity.of(Optional.of(new ProductV2("HP Laptop", BigDecimal.TEN)));
+    }
+
+    //Header Versioning (Request Header)
+    //http://localhost:8080/api/header/product
+    @GetMapping(value = "/header/product", headers = "X-API-VERSION=1")
+    public ResponseEntity<ProductV1> headerVersioningProductV1() {
+        return ResponseEntity.of(Optional.of(new ProductV1("HP Laptop")));
+    }
+    //http://localhost:8080/api/header/product
+    @GetMapping(value = "/header/product", headers = "X-API-VERSION=2")
+    public ResponseEntity<ProductV2> headerVersioningProductV2() {
+        return ResponseEntity.of(Optional.of(new ProductV2("HP Laptop", BigDecimal.TEN)));
+    }
+}
---
+++ src/main/java/com/rouvsen/springflywayintro/apiversioning/entity/Product.java
@@ -0,0 +1,28 @@
+package com.rouvsen.springflywayintro.apiversioning.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @SequenceGenerator(
+            name = "product_sequence",
+            sequenceName = "product_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_sequence"
+    )
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
